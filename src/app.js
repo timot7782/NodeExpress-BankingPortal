7e605742f4cb25f7c1657db7111af3726370f7be
Timothy Trisnadhama <tim_trisnadhama@manulife.com>
2019-10-02 23:59:10 -0400

    finish module 4

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8' });
-const accounts = JSON.parse(accountData);
+const { accounts, users, writeJSON } = require('./data');
 
-const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'utf8' });
-const users = JSON.parse(userData);
+// const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8' });
+// const accounts = JSON.parse(accountData);
+
+// const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'utf8' });
+// const users = JSON.parse(userData);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -44,9 +46,7 @@ app.post('/transfer', (req, res) => {
     accounts[from].balance -= parseInt(amount);
     accounts[to].balance += parseInt(amount);
   }
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-
+  writeJSON();
   res.render('transfer', { message: "Transfer Completed" });
 });
 
@@ -57,8 +57,7 @@ app.post('/payment', (req, res) => {
   const { amount } = req.body;
   accounts.credit.balance -= parseInt(amount);
   accounts.credit.available += parseInt(amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('payment', { message: "Payment Successful", account: accounts.credit });
 })
 
