f97031926a0046dca96124d3b753622f987a828d
Timothy Trisnadhama <tim_trisnadhama@manulife.com>
2019-10-02 23:19:46 -0400

    finish module2

--- src/app.js
+++ src/app.js
@@ -6,13 +6,33 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'UTF8' });
+const users = JSON.parse(userData);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
 });
 
 app.listen(port, () => console.log(`PS Project Running on port ${port}!`));
